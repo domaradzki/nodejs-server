2aaa1c180a10f83220f8b8ebbe994fb94b891a72
Grzegorz <domaradzki.g@gmail.com>
2020-01-04 23:04:01 +0100

    implemented express

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>My site</title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
+const app = express();
 
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My site</title></head>");
-  res.write("<body><h1>Your welcome</h1></body>");
-  res.write("</html>");
-  return res.end();
+app.use((req, res, next) => {
+  console.log("In the midleware");
+  next();
 });
 
-server.listen(7000, console.log("Server started"));
+app.use((req, res, next) => {
+  console.log("In the next midleware");
+  res.send("<h1>Hello Server Express!!</h1>");
+});
+
+app.listen(3000, console.log("Server started"));
