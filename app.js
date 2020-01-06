ae6a58b1a0e71e0811726a240abc0eebe226c5d2
Grzegorz <domaradzki.g@gmail.com>
2020-01-06 15:37:04 +0100

    added router

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the midleware");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  console.log("In the next midleware");
-  res.send("<h1>Hello Server Express!!</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000, console.log("Server started"));
