3bd96ec0c88712900dc0cd3833703eae5c8eda94
Grzegorz <domaradzki.g@gmail.com>
2020-01-06 20:24:02 +0100

    implemented PUG as template dynamic html files

--- routes/shop.js
+++ routes/shop.js
@@ -2,12 +2,15 @@ const path = require("path");
 
 const express = require("express");
 
-const router = express.Router();
-
 const rootDir = require("../utils/path");
 
+const router = express.Router();
+
+const adminData = require("./admin");
+
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
